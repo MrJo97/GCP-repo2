b3fa1e4cf30c0bc230ceaca39ce33b709a592c9c
MrJo97 <papaalex97@outlook.it>
2026-09-17 00:13:48 +0200

    Changes to cloudbuild.yaml file

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Welcome to python Flask world - new change V1.0 fromk CI/CD pipeline'
+    return 'Welcome to python Flask world - new change V2.0 from CI/CD pipeline'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
